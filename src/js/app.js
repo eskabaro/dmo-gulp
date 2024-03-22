eb3f720f8b783557b12293f8d23c1f921d422572
eskabaro <kononucenkosasa4004@gmail.com>
2024-03-22 19:51:16 +0200

    created service-overview section

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
 import * as flsFunctions from "./modules/functions.js";
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
 
 flsFunctions.isWebp();
+
+new Swiper(".service_overview__swiper", {
+    modules: [Pagination],
+    loopAdditionalSlides: 2,
+    initialSlide: 5,
+    loop: true,
+    // rewind: true,
+    slidesPerView: "auto",
+    slideToClickedSlide: true,
+    slideActiveClass: "active",
+    pagination: {
+        el: ".service_overview__pagination",
+        type: "bullets",
+        clickable: true,
+    },
+});
