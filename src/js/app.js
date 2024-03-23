1184b21c59b24410613822362778a4d926ae25aa
eskabaro <kononucenkosasa4004@gmail.com>
2024-03-23 16:15:50 +0200

    created first slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,20 @@
 import * as flsFunctions from "./modules/functions.js";
 import Swiper from "swiper";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 
 flsFunctions.isWebp();
 
+const listBox = document.querySelectorAll(".slide");
+
 new Swiper(".service_overview__swiper", {
-    modules: [Pagination],
+    modules: [Pagination, Autoplay],
     loopAdditionalSlides: 2,
     initialSlide: 5,
     loop: true,
-    // rewind: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     slidesPerView: "auto",
     slideToClickedSlide: true,
     slideActiveClass: "active",
@@ -17,5 +22,15 @@ new Swiper(".service_overview__swiper", {
         el: ".service_overview__pagination",
         type: "bullets",
         clickable: true,
+    },
+    onAny(arg, ...props) {
+        listBox.forEach((item) => {
+            const listH = item.children[1].children[1].offsetHeight;
+            item.children[1].style.bottom = `-${listH + (listH ? 20 : 0)}px`;
+
+            if (window.innerWidth >= 768 && item.classList.contains("active")) {
+                item.children[1].style.bottom = "0px";
+            }
+        });
     },
 });
