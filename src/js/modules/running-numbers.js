80fef1b2ecb30c8639e529d61e09b03de8872a98
eskabaro <kononucenkosasa4004@gmail.com>
2024-03-27 16:02:16 +0200

    created counters animated

--- src/js/modules/running-numbers.js
+++ src/js/modules/running-numbers.js
@@ -8,10 +8,15 @@ const outNum = (num, elem) => {
     const l = document.querySelector(`#${elem}`);
     const t = Math.round(time / (num / step));
 
+    l.classList.add("active");
+
     const interval = setInterval(() => {
         n = n + step;
 
-        if (n == num) clearInterval(interval);
+        if (n == num) {
+            clearInterval(interval);
+            l.classList.remove("active");
+        }
 
         l.innerHTML = n >= 7000 ? `${n}+` : n;
     }, t);
